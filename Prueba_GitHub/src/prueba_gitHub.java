9dd69c1e325f313327731aadc9be8ce2debaa685
alejandra-cmd <carpediem080621@gmail.com>
2020-02-06 13:24:45 +0100

    Ajuste realizado.
    Signed-off-by: alejandra-cmd <carpediem080621@gmail.com>

--- Prueba_GitHub/src/prueba_gitHub.java
+++ Prueba_GitHub/src/prueba_gitHub.java
@@ -5,7 +5,9 @@ public class prueba_gitHub {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola mundo");	
+		System.out.println("Hola mundo");
+		System.out.println("Prueba2");
+		System.out.println("Prueba3");
 		
 
 	}
